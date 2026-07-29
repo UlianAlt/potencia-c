8137774a8ca55691fe6554fc0d2128cdcccad7d8
UlianAlt <zpedro237732@gmail.com>
2026-07-29 19:43:10 -0300

    Log output to file 'potencia.txt'
    Added file output for program instructions and results.

--- potencia.c
+++ potencia.c
@@ -4,15 +4,24 @@
 
 int main(int argc, char const *argv[])
 {
+    FILE *arquivo = fopen("potencia.txt", "w");
+
 	if(argc != 3){
         printf("digite o nome do programa seguido de 2 argumentos (ex: potencia 5 2)");
         printf("\nsendo 5 a base e 2 o expoente, esses valores podem ser quaisquer inteiros");
+
+        fprintf(arquivo, "digite o nome do programa seguido de 2 argumentos (ex: potencia 5 2)");
+        fprintf(arquivo, "\nsendo 5 a base e 2 o expoente, esses valores podem ser quaisquer inteiros");
     }else{
     	double num1 = atof(argv[1]);
     	double num2 = atof(argv[2]);
 		printf("resultado: %f\n", potencia(num1, num2));
 		printf("\nFeito por PedroUlian (https://github.com/PedroUlian) (https://github.com/UlianAlt)");
+
+        fprintf(arquivo, "resultado: %f\n", potencia(num1, num2));
+		fprintf(arquivo, "\nFeito por PedroUlian (https://github.com/PedroUlian) (https://github.com/UlianAlt)");
     }
 
+    fclose(arquivo);
     return 0;
 }
